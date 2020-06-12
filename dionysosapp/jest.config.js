59b1d3588a5bd7a444037921fa22d013f478b449
shaman-apprentice <Torsten.Knauf@gmx.de>
2020-06-12 17:12:16 +0200

    Fix jest

---
+++ dionysosapp/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "transformIgnorePatterns": [
+    "<rootDir>/node_modules/(?!react-router-native|react-native-elements|(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)",
+  ],
+  "preset": "react-native",
+  "moduleNameMapper": {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
+  }
+}
