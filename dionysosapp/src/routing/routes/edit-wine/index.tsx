f8bf331317003070c166cbcaabe586565f00fab0
shaman-apprentice <Torsten.Knauf@gmx.de>
2020-08-11 16:41:52 +0200

    PoC for `Formik` on all 3 platforms

--- dionysosapp/src/routing/routes/edit-wine/index.tsx
+++ dionysosapp/src/routing/routes/edit-wine/index.tsx
@@ -4,13 +4,13 @@ import { Text } from 'react-native';
 import { useParams } from '../../router';
 import { AzureContext } from '../../../azure-api/AzureContext';
 import { Wine } from '../../../types/wine';
+import { WineForm } from './WineForm';
 
 export default function EditWine() {
   const { wines } = useContext(AzureContext);
 
   const { RowKey } = useParams()
-  console.log('RowKey:', RowKey);
-  console.log('Wines:', wines[RowKey] );
+
   const wine: Wine = wines[RowKey] ?? {
     PartitionKey: '1',
     RowKey: '-1',
@@ -20,5 +20,5 @@ export default function EditWine() {
     sweetness: 'medium dry',
   }
 
-  return <Text>{JSON.stringify(wine, null, 2)}</Text>
+  return <WineForm wine={wine} />
 }
