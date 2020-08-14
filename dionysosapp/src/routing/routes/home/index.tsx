d2ddd8397dbe85bd0a4c22fc02ca64dcd350f45d
shaman-apprentice <Torsten.Knauf@gmx.de>
2020-08-14 13:02:52 +0200

    Add some padding to home as missclicking was easy

--- dionysosapp/src/routing/routes/home/index.tsx
+++ dionysosapp/src/routing/routes/home/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
 import { FontAwesome5 } from '@expo/vector-icons';
 
 import { Link } from '../../router';
@@ -8,11 +8,25 @@ export default function Home() {
   return <>
     <FontAwesome5 name="glass-cheers" size={32} color="red" />
 
-    <Link to="/wine-overview">
-      <Text>View wines</Text>
-    </Link>
-    <Link to="/edit-wine/-1">
-      <Text>Add a wine</Text>
-    </Link>
+    <View style={styles.link}>
+      <Link  to="/wine-overview">
+        <Text>View wines</Text>
+      </Link>
+    </View>
+    <View style={styles.link}>
+      <Link to="/edit-wine/-1">
+        <Text>Add a wine</Text>
+      </Link>
+    </View>
   </>;
 }
+
+const styles = StyleSheet.create({
+  link: {
+    borderWidth: 2,
+    borderStyle: 'solid',
+    borderColor: 'blue',
+    padding: 8,
+    marginTop: 8,
+  },
+});
