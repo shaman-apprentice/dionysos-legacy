ce4e425b4dfcd42b05b2a2001e85e0d582cc7611
shaman-apprentice <Torsten.Knauf@gmx.de>
2020-08-30 11:41:49 +0200

    Remove obsolete comment
    as login throws now in case of invalid credentials

--- dionysosapp/src/azureApi/IAzureContext.ts
+++ dionysosapp/src/azureApi/IAzureContext.ts
@@ -2,7 +2,7 @@ import { Wine } from '../types/wine';
 
 export interface IAzureContext {
   isLoggedIn: boolean;
-  login: (user: string, pw: string) => Promise<void | string>; // string in error case with error msg
+  login: (user: string, pw: string) => Promise<void>
   wines: {[RowKey: string]: Wine};
   saveWine: (wine: Wine) => Promise<void>;
 }
